e90e526e9ccb1d4f155f363ca67fc1237786710a
Your Name <you@example.com>
2023-10-06 21:44:24 -0400

    feat: add buttons to html  snake crawler function

--- app.js
+++ app.js
@@ -1,42 +1,31 @@
-console.log('this console works');
+// set global variables for game play display, canvas, snake, and food
+let movementDisplay;
+let ctx
+let game;
+let snake;
+let apple; //food
 
+ // set global variables for snake length, score, snake movement, and buttons
+let random_x;
+let random_y;
+let score = 0;
+let highScore = 0;
+let snakeArray = [];
+let restartBtn = document.getElementById('restartBtn');
+let playBtn = document.getElementById('playBtn');
 
-// create variable for board size and dimension for the snake game
-const blockSize = 25;
-const rowTotal = 20;
-const columnTotal = 20;
-const board;
-const context
-
-// create variable for coordinate for the snake 
-const snakeX = blockSize * 3;
-const snakeY = blockSize * 3;
-
-// create variable for starting location for the snake
-const speedX = 0;
-const speedY = 0;
-
-// create array variable for the snake body
-const snakeBody = [];
-
-// create variable for snake food
-const appleX;
-const appleY;
-
-// create variable to game ending; return boolean
-const gameOver = false;
-
-// run function when window open up
-window.onload = function () {
-    // create game on window, give it height and width
-    game = document.getElementById("game");
-    game.height = rowTotal * blockSize;
-    game.width = columnTotal * blockSize;
-    // set the game dimension to 2d
-    context = game.getContext("2d");
-    // create function for the food to spawn
-    foodSpot();
-    document.addEventListener("keyup" || "w", changeDirection);
-    // set the snake speed
-    setInterval(update, 1000);
-}
+// create crawler function for snake and snake movement
+function Crawler(x, y, width, height, color) {
+  this.x = x;
+  this.y = y;
+  this.speedX = 0;
+  this.speedY = 10;
+  this.width = width;
+  this.height = height;
+  this.color = color;
+  this.alive = true;
+//   this.snakeBody = [];
+  this.render = function() {
+    ctx.fillStyle = this.color;
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
